c710202867f60bf287c0503541333a1d8334b50b
Karen Gutierrez <GutierrezKaren1007@gmail.com>
2020-07-27 18:19:49 -0700

    Complete Iteration 0

---
+++ ToDoLIstPro/ToDo.swift
@@ -0,0 +1,18 @@
+//
+//  ToDo.swift
+//  ToDoLIstPro
+//
+//  Created by Karen Gutierrez  on 7/27/20.
+//  Copyright © 2020 Karen Gutierrez . All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name = ""
+    var important = false
+    
+}
+
+
